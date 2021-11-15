3d2f907548bb95d9f3afec7968e70aff56af509d
3x10RaiseTo8 <abhisheks1177@gmail.com>
2021-11-15 22:21:52 +0530

    Add more comments

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # typing-speed-test
 
-This is a basic Command Line Interface (CLI) application which records typing speed of the user in Words per Minute (WPM). The application randomizes text from text.txt file and measures typing speed in runtime. The test is successful only if the user enters the exact text match, hence mistakes are NOT ignored.
+This is a basic Command Line Interface (CLI) application which records typing speed of the user in Words per Minute (WPM). The application randomizes text from `text.txt` file and measures typing speed in runtime. The test is successful only if the user enters the exact text match, hence mistakes are NOT ignored. Based on `curses` module.
 
 ## How to use
-Download and extract this repository in a folder. Open any terminal application and navigate to that folder. Run main.py file in Python 3.x. Press escape to exit.
 
-Add new lines in text.txt to customise test.
+Download and extract this repository in a folder. Open any terminal application and navigate to that folder. Run `main.py` file in Python `3.x`. Press `escape` to exit.
+
+`Curses` module comes preinstalled with Python on Mac and Linux, but not on Windows. For running this code on Windows machine, do `pip install windows-curses` first.
+
+Add new lines in `text.txt` to customise test.
--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import random
 
 
 def main(stdscr):
+
+    # Initialization of color pairs
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)  # Correct
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_RED)  # Wrong
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Normal
 
+    # The welcome screen
     welcome(stdscr)
 
+    # Gives test until user presses ESC
     while True:
         test(stdscr)
         stdscr.addstr(4, 0,
@@ -28,15 +32,20 @@ def welcome(stdscr):
 
 
 def get_text():
-    with open("texts.txt") as f:
+
+    # Gets random text from a file
+    with open("text.txt") as f:
         lines = f.readlines()
         return random.choice(lines).strip()
 
 
 def display(stdscr, given, userinput, wpm):
+
+    # Displays text to be written and live WPM
     stdscr.addstr(given)
     stdscr.addstr(2, 0, f"WPM: {wpm}")
 
+    # Logic for correct and incorrect character input
     for i, char in enumerate(userinput):
         color = curses.color_pair(
             1) if given[i] == char else curses.color_pair(2)
@@ -44,37 +53,51 @@ def display(stdscr, given, userinput, wpm):
 
 
 def test(stdscr):
+
+    # Loading essential variables in memory
     given = get_text()
     userinput = []
     wpm = 0
     start = time()
 
+    # Constantly asks for input, returns error if not provided (Required for LIVE update of WPM)
     stdscr.nodelay(True)
 
     while True:
+
+        # Word per minute calculation using time module
         elapsed = max(time() - start, 1)
         wpm = round((len(userinput) / (elapsed / 60)) / 5)
 
+        # Live update of text on screen
         stdscr.clear()
         display(stdscr, given, userinput, wpm)
         stdscr.refresh()
 
+        # Checks if the user completed the test
         if "".join(userinput) == given:
             stdscr.nodelay(False)
             break
 
+        # If no key input, stop the error and continue
         try:
             key = stdscr.getkey()
         except:
             continue
 
+        # Escape to quit
         if ord(key) == 27:
             break
+
+        # Backspace to delete
         elif key in ["KEY_BACKSPACE", "\b", "\x7f"]:
             if userinput:
                 userinput.pop()
+
+        # User cannot enter more characters that the given text
         elif len(userinput) < len(given):
             userinput.append(key)
 
 
+# Calling the main function in a wrapper which sets up curses module
 wrapper(main)
